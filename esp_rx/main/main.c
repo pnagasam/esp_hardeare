62a2c70e17b472666dbffe559d578337fcb23b0b
pnagasam <pnagasam@ucsd.edu>
2026-04-13 16:50:06 -0700

    trying to get it working

--- esp_rx/main/main.c
+++ esp_rx/main/main.c
@@ -6,13 +6,15 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_netif.h"
-#include "esp_now.h"
 #include "esp_timer.h"
 #include "nvs_flash.h"
+#include "lwip/sockets.h"
+#include "lwip/inet.h"
 
 static const char *TAG = "rx";
 
-#define AP_SSID "esp-beacon"
+#define AP_SSID     "esp-beacon"
+#define BEACON_PORT 5000
 
 static EventGroupHandle_t wifi_evt;
 #define GOT_IP_BIT BIT0
@@ -32,33 +34,58 @@ static void event_handler(void *arg, esp_event_base_t base,
     }
 }
 
+static void promiscuous_rx_cb(void *buf, wifi_promiscuous_pkt_type_t type) {
+    (void)buf;
+    (void)type;
+}
+
 static void csi_cb(void *ctx, wifi_csi_info_t *info) {
     if (!info || !info->buf) return;
     int64_t ts = esp_timer_get_time();
     const int8_t *csi = info->buf;
     uint16_t len = info->len;
-
     printf("CSI,%lld,%d,%d,%d,%u,"
            "%02X:%02X:%02X:%02X:%02X:%02X,",
            ts, info->rx_ctrl.rssi, info->rx_ctrl.rate,
            info->rx_ctrl.channel, len,
            info->mac[0], info->mac[1], info->mac[2],
            info->mac[3], info->mac[4], info->mac[5]);
+    
     for (uint16_t i = 0; i < len; i++) {
-        printf("%d%c", csi[i], (i + 1 == len) ? '\n' : ' ');
+      printf("%d%c", csi[i], (i + 1 == len) ? '\n' : ' ');
     }
     csi_count++;
+
 }
 
-static void espnow_recv_cb(const esp_now_recv_info_t *recv_info,
-                           const uint8_t *data, int data_len) {
-    beacon_count++;
-}
+static void udp_recv_task(void *arg) {
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "socket create failed: %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
 
-static void espnow_init(void) {
-    ESP_ERROR_CHECK(esp_now_init());
-    ESP_ERROR_CHECK(esp_now_register_recv_cb(espnow_recv_cb));
-    ESP_LOGI(TAG, "ESP-NOW initialized");
+    struct sockaddr_in bind_addr = {
+        .sin_family      = AF_INET,
+        .sin_port        = htons(BEACON_PORT),
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+    };
+    if (bind(sock, (struct sockaddr *)&bind_addr, sizeof(bind_addr)) < 0) {
+        ESP_LOGE(TAG, "bind failed: %d", errno);
+        close(sock);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    char buf[64];
+    while (1) {
+        int len = recv(sock, buf, sizeof(buf), 0);
+        if (len > 0) {
+          printf("Got UDP Beacon\n");
+            beacon_count++;
+        }
+    }
 }
 
 static void wifi_init_sta(void) {
@@ -66,6 +93,7 @@ static void wifi_init_sta(void) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -82,36 +110,48 @@ static void wifi_init_sta(void) {
             .threshold.authmode = WIFI_AUTH_OPEN,
         },
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    wifi_config_t ap = {
+        .ap = {
+            .ssid           = "esp-rx-dummy",
+            .ssid_len       = strlen("esp-rx-dummy"),
+            .channel        = 1,
+            .max_connection = 1,
+            .authmode       = WIFI_AUTH_OPEN,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap));
+    ESP_ERROR_CHECK(esp_wifi_set_bandwidth(WIFI_IF_AP, WIFI_BW40));
+    ESP_ERROR_CHECK(esp_wifi_set_bandwidth(WIFI_IF_STA, WIFI_BW40));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI(TAG, "connecting to %s...", AP_SSID);
-    xEventGroupWaitBits(wifi_evt, GOT_IP_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-    esp_wifi_set_ps(WIFI_PS_NONE);
-    ESP_LOGI(TAG, "connected");
-
-    ESP_ERROR_CHECK(esp_wifi_set_bandwidth(WIFI_IF_STA, WIFI_BW20));
-
     wifi_promiscuous_filter_t filter = {
-        .filter_mask = WIFI_PROMIS_FILTER_MASK_MGMT,
+        .filter_mask = WIFI_PROMIS_FILTER_MASK_MGMT
+                     | WIFI_PROMIS_FILTER_MASK_DATA,
     };
-    esp_wifi_set_promiscuous_filter(&filter);
-    esp_wifi_set_promiscuous(true);
+    ESP_ERROR_CHECK(esp_wifi_set_promiscuous_filter(&filter));
+    ESP_ERROR_CHECK(esp_wifi_set_promiscuous_rx_cb(promiscuous_rx_cb));
+    ESP_ERROR_CHECK(esp_wifi_set_promiscuous(true));
 
     wifi_csi_config_t csi_cfg = {0};
-    csi_cfg.lltf_en          = true;
-    csi_cfg.htltf_en         = true;
-    csi_cfg.stbc_htltf2_en   = true;
-    csi_cfg.ltf_merge_en     = true;
+    csi_cfg.lltf_en           = true;
+    csi_cfg.htltf_en          = true;
+    csi_cfg.stbc_htltf2_en    = true;
+    csi_cfg.ltf_merge_en      = false;
     csi_cfg.channel_filter_en = true;
-    csi_cfg.manu_scale       = false;
-    csi_cfg.shift            = 0;
+    csi_cfg.manu_scale        = false;
+    csi_cfg.shift             = 0;
+    csi_cfg.dump_ack_en       = false;
 
-    ESP_ERROR_CHECK(esp_wifi_set_csi_config(&csi_cfg));
     ESP_ERROR_CHECK(esp_wifi_set_csi_rx_cb(&csi_cb, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_csi_config(&csi_cfg));
     ESP_ERROR_CHECK(esp_wifi_set_csi(true));
-    ESP_LOGI(TAG, "CSI enabled");
+    ESP_LOGI(TAG, "CSI armed; connecting to %s...", AP_SSID);
+
+    xEventGroupWaitBits(wifi_evt, GOT_IP_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_LOGI(TAG, "connected");
 }
 
 static void heartbeat_task(void *arg) {
@@ -125,6 +165,6 @@ static void heartbeat_task(void *arg) {
 void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
     wifi_init_sta();
-    espnow_init();
-    xTaskCreate(heartbeat_task, "hb", 2048, NULL, 3, NULL);
+    xTaskCreate(udp_recv_task,  "udp_rx",    4096, NULL, 5, NULL);
+    xTaskCreate(heartbeat_task, "hb",        2048, NULL, 3, NULL);
 }
--- esp_tx/main/main.c
+++ esp_tx/main/main.c
@@ -5,22 +5,23 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_netif.h"
-#include "esp_now.h"
 #include "nvs_flash.h"
+#include "lwip/sockets.h"
+#include "lwip/inet.h"
 
 static const char *TAG = "tx";
 
 #define AP_SSID        "esp-beacon"
-#define AP_CHAN        6
-#define BEACON_RATE_MS 200
-
-static const uint8_t BROADCAST_MAC[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-static const uint8_t BEACON_PAYLOAD[] = "BEACON";
+#define AP_CHAN        1
+#define BEACON_RATE_MS 20
+#define BEACON_PORT    5000
+#define BEACON_PAYLOAD "BEACON"
 
 static void wifi_init_softap(void) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_ap();
+    esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -34,47 +35,50 @@ static void wifi_init_softap(void) {
             .authmode       = WIFI_AUTH_OPEN,
         },
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    wifi_config_t sta = { 0 };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta));
     ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_LOGI(TAG, "softAP '%s' up on channel %d", AP_SSID, AP_CHAN);
+    ESP_ERROR_CHECK(esp_wifi_set_bandwidth(WIFI_IF_AP, WIFI_BW40));
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_LOGI(TAG, "softAP '%s' up on channel %d (HT40)", AP_SSID, AP_CHAN);
 }
 
-static void espnow_init(void) {
-    ESP_ERROR_CHECK(esp_now_init());
-
-    esp_now_peer_info_t peer = {
-        .channel = AP_CHAN,
-        .ifidx   = WIFI_IF_AP,
-        .encrypt = false,
-    };
-    memcpy(peer.peer_addr, BROADCAST_MAC, 6);
-    ESP_ERROR_CHECK(esp_now_add_peer(&peer));
-    ESP_LOGI(TAG, "ESP-NOW initialized, broadcast peer added");
-}
-
-static bool any_sta_connected(void) {
-    wifi_sta_list_t sta_list = {0};
-    if (esp_wifi_ap_get_sta_list(&sta_list) != ESP_OK) return false;
-    return sta_list.num > 0;
+static void mac_print_task(void *arg) {
+    uint8_t mac[6];
+    esp_wifi_get_mac(WIFI_IF_AP, mac);
+    while (1) {
+        ESP_LOGI(TAG, "AP MAC: %02X:%02X:%02X:%02X:%02X:%02X",
+                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+        vTaskDelay(pdMS_TO_TICKS(10000));
+    }
 }
 
 static void beacon_task(void *arg) {
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "socket create failed: %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    int broadcast = 1;
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+
+    struct sockaddr_in dest = {
+        .sin_family      = AF_INET,
+        .sin_port        = htons(BEACON_PORT),
+        .sin_addr.s_addr = inet_addr("255.255.255.255"),
+    };
+
     while (1) {
         TickType_t tick_start = xTaskGetTickCount();
 
-        if (!any_sta_connected()) {
-            ESP_LOGI(TAG, "no STA connected yet, waiting...");
-            vTaskDelayUntil(&tick_start, pdMS_TO_TICKS(3000));
-            continue;
-        }
-
-        esp_err_t err = esp_now_send(BROADCAST_MAC, BEACON_PAYLOAD,
-                                     sizeof(BEACON_PAYLOAD) - 1);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "esp_now_send failed: %d", err);
-        } else {
-            ESP_LOGI(TAG, "beacon sent via ESP-NOW broadcast");
+        int ret = sendto(sock, BEACON_PAYLOAD, sizeof(BEACON_PAYLOAD) - 1, 0,
+                         (struct sockaddr *)&dest, sizeof(dest));
+        if (ret < 0) {
+            ESP_LOGE(TAG, "sendto failed: %d", errno);
         }
 
         vTaskDelayUntil(&tick_start, pdMS_TO_TICKS(BEACON_RATE_MS));
@@ -84,6 +88,6 @@ static void beacon_task(void *arg) {
 void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
     wifi_init_softap();
-    espnow_init();
-    xTaskCreate(beacon_task, "beacon", 4096, NULL, 5, NULL);
+    xTaskCreate(beacon_task,    "beacon",    4096, NULL, 5, NULL);
+    xTaskCreate(mac_print_task, "mac_print", 2048, NULL, 3, NULL);
 }
